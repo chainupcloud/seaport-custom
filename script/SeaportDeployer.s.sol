912e73301a1597ea218430481eb07343012a7778
RenShiWei <ren_shi_wei@qq.com>
2024-03-08 17:29:53 +0800

    init

--- script/SeaportDeployer.s.sol
+++ script/SeaportDeployer.s.sol
@@ -12,7 +12,8 @@ interface ImmutableCreate2Factory {
     ) external payable returns (address deploymentAddress);
 }
 
-// FOUNDRY_PROFILE=optimized forge script script/SeaportDeployer.s.sol:SeaportDeployer  --rpc-url https://api.chainup.net/ethereum/holesky/ed670f52117b4eea8ccf9310b77ca0e5 --broadcast --verify --retries 10 --delay 30
+// forge script script/SeaportDeployer.s.sol:SeaportDeployer --skip test/  --rpc-url https://api.chainup.net/ethereum/holesky/ed670f52117b4eea8ccf9310b77ca0e5 --broadcast --verify --retries 10 --delay 30
+// forge verify-contract 0x32Fe9BF355a38Dd85c53B76Bb296f026066b2e0b Seaport --chain 17000 --constructor-args $(cast abi-encode "constructor(address)" 0x00000000F9490004C11Cef243f5400493c00Ad63) --etherscan-api-key MHVDXC3S36YTS89GXQZFB5HBVRCZ82BZTY --watch
 // NOTE: This script assumes that the CREATE2-related contracts have already been deployed.
 contract SeaportDeployer is Script {
     ImmutableCreate2Factory private constant IMMUTABLE_CREATE2_FACTORY =
@@ -27,7 +28,7 @@ contract SeaportDeployer is Script {
         vm.startBroadcast(vm.envUint("PRIVATE_KEY"));
 
         // CREATE2 salt (20-byte caller or zero address + 12-byte salt).
-        bytes32 salt = 0x0000000000000000000000000000000000000000aaaaaaaaaaaaaaaaaaaaaaaa;
+        bytes32 salt = 0x0000000000000000000000000000000000000000aaaaaaaaaaaaaaaaaaaaaaaf;
 
         // Packed and ABI-encoded contract bytecode and constructor arguments.
         // NOTE: The Seaport contract *must* be compiled using the optimized profile config.
@@ -38,6 +39,8 @@ contract SeaportDeployer is Script {
 
         // Deploy the Seaport contract via ImmutableCreate2Factory.
         address seaport = IMMUTABLE_CREATE2_FACTORY.safeCreate2(salt, initCode);
+
+        console.log("seaportAddress:", seaport);
 
         // Verify that the deployed contract address matches what we're expecting.
 //        assert(seaport == SEAPORT_ADDRESS);
