67297a79a0f2281a92593433fe897abed9ccb302
RenShiWei <ren_shi_wei@qq.com>
2024-03-21 16:57:58 +0800

    deploy config

--- script/CustomSeaportDeployer.s.sol
+++ script/CustomSeaportDeployer.s.sol
@@ -7,8 +7,8 @@ import {Seaport} from "contracts/Seaport.sol";
 import {ConduitController} from "contracts/conduit/ConduitController.sol";
 
 // forge script script/CustomSeaportDeployer.s.sol:SeaportAllDeployer --rpc-url https://api.chainup.net/ethereum/holesky/$ --broadcast --retries 10 --delay 30
-// forge verify-contract 0x2D206f3f2d0c97586dcf40c5095BEEe33B34D347 Seaport --chain 17000 --constructor-args $(cast abi-encode "constructor(address)" 0x3c8116B4ac0db545e184e2dc9CBB2Eb67a4dEd3F) --etherscan-api-key $ --watch
-// forge verify-contract 0x3c8116B4ac0db545e184e2dc9CBB2Eb67a4dEd3F ConduitController --chain 17000 --etherscan-api-key $ --watch
+// forge verify-contract 0x12246eb75caF4D4FBFcE48741abC5EeDCB1c1B76 Seaport --chain 17000 --constructor-args $(cast abi-encode "constructor(address)" 0x93a6860F2F089cb88B9c9355CD4A6E269B7FDc93) --etherscan-api-key $ --watch
+// forge verify-contract 0x93a6860F2F089cb88B9c9355CD4A6E269B7FDc93 ConduitController --chain 17000 --etherscan-api-key $ --watch
 // NOTE: This script assumes that the CREATE2-related contracts have already been deployed.
 // verify 会有问题
 contract SeaportAllDeployer is Script {
